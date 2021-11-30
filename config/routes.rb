7626379d14ac5bcfe7adb8ca9170e5f87c0e677d
Charles (Chaz) Formichella <74632592+chazpf@users.noreply.github.com>
2021-11-30 13:17:48 -0500

    events model created, routing and api controller begun

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  # resources :events
   get 'hello_world', to: 'hello_world#index'
+
+  namespace :api do
+    namespace :v1 do
+      get 'events/index'
+      post 'events/create'
+      delete 'events/:id', to: 'events#destroy'
+    end
+  end
+  root "events#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "hello_world#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_11_30_173935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "label"
+    t.string "day"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
