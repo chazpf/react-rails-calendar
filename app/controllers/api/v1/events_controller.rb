222a83f10bfbe79f0b4a2965952fd22aac5b9111
Charles (Chaz) Formichella <74632592+chazpf@users.noreply.github.com>
2021-12-01 17:06:53 -0500

    trying something else to fix heroku

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::EventsController < ApplicationController
-  skip_before_action :verify_authenticity_token
   before_action :set_event, only: %i[ show edit update destroy ]
 
   # GET /events or /events.json
