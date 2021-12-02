e01199f680bb094d25c16895ccff8202e60dfc49
Charles (Chaz) Formichella <74632592+chazpf@users.noreply.github.com>
2021-12-01 21:11:49 -0500

    csrf attempt to solve 422 error

--- client/pages/Signup.js
+++ client/pages/Signup.js
@@ -23,8 +23,8 @@ const Signup = ({handleLogin}) => {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    const token = document.querySelector('[name=csrf-token]').content;
-    axios.defaults.headers.common['X-CSRF-TOKEN'] = token;
+    // const token = document.querySelector('[name=csrf-token]').content;
+    // axios.defaults.headers.common['X-CSRF-TOKEN'] = token;
 
     const user = {
       username,
