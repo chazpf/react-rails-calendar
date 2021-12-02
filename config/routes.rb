9ade632c2169632fb9a389eef3e2cfb9eafe0bab
Charles (Chaz) Formichella <74632592+chazpf@users.noreply.github.com>
2021-12-01 20:49:31 -0500

    csrf attempt to solve 422 error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
-  post '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
   get '/logged_in', to: 'sessions#is_logged_in?'
 
   # resources :events
