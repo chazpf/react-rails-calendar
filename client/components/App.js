1361399afb8c55c2aa4b923280e06e3ab775c514
Charles (Chaz) Formichella <74632592+chazpf@users.noreply.github.com>
2021-12-01 21:15:25 -0500

    undo last commit

--- client/components/App.js
+++ client/components/App.js
@@ -16,8 +16,8 @@ const App = () => {
   };
 
   const handleLogout = () => {
-    // const token = document.querySelector('[name=csrf-token]').content;
-    // axios.defaults.headers.common['X-CSRF-TOKEN'] = token;
+    const token = document.querySelector('[name=csrf-token]').content;
+    axios.defaults.headers.common['X-CSRF-TOKEN'] = token;
 
     axios.post('/logout', {withCredentials:true})
       .then(response => {
